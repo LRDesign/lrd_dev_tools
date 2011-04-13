8ec3b36058b2351ecbbfee3efcb36a9f09f10764
Evan Dorn <evan@lrdesign.com>
2011-04-12 20:58:04 -0700

    nearly working template system

--- lib/application_template.rb
+++ lib/application_template.rb
@@ -5,16 +5,25 @@ gem 'mizugumo'
 gem 'will_paginate', "~> 3.0.pre2"
 gem 'populator'
 gem 'faker'
+gem 'mysql2'
 
-gem 'rspec',              :group => "development test"
-gem 'rspec-rails',        :group => "development test"
-gem 'factory_girl_rails', :group => "development test"
-gem 'webrat',             :group => 'development test'
-gem 'ruby-debug'
+append_to_file('Gemfile') do
+<<-EOTEXT
+group :development, :test do
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'webrat'
+  gem 'ruby-debug'
+end
 
-gem 'annotate',      :group => 'development'
-gem 'lrd_dev_tools', :group => 'development', :path => '../lrd_dev_tools'
-gem 'mongrel',       :group => 'development'
+group :development do
+  gem 'annotate'
+  gem 'lrd_dev_tools', :path => '../lrd_dev_tools'
+  gem 'mongrel'
+end
+EOTEXT
+end
 
 # Delete prototype
 inside('public/javascripts') do
@@ -28,6 +37,8 @@ inside 'app/views/layouts' do
   remove_file('application.html.erb')
 end
 template "application.html.haml", "app/views/layouts/application.html.haml"
+template "_nav.html.haml", "app/views/layouts/_nav.html.haml"
+template "_flash.html.haml", "app/views/layouts/_flash.html.haml"
 
 # template some more files
 remove_file '.gitignore'
@@ -60,12 +71,13 @@ remove_dir('test')
 
 # TODO template sample_data.rake
 # TODO template README
+# TODO template spec/support files
+# TODO template standard partials:  nav, flash, etc.
 
 # run installs
 run 'bundle install'
 run 'rails generate mizugumo:install'
 run 'rails generate rspec:install'
-
 
 # TODO - LONG TERM below this line
 
