e823c7470a16fb77f5bc91b92aa076c885e9dc19
Evan Dorn <evan@lrdesign.com>
2011-04-25 17:02:35 -0700

    added default capistrano config to template

---
+++ lib/templates/rails_app/config/deploy/production.rb
@@ -0,0 +1,10 @@
+set :runner, '<USER_NAME>'
+set :user, '<USER_NAME>'
+
+set :domain, '<DOMAIN>'
+set :application, '<APPLICATION>'      # eg 'rfx'
+set :deploy_to, '<PATH>'
+set :keep_releases, 10
+set :branch, 'production'
+set :rails_env, "production"
+set :use_sudo, false
---
+++ lib/templates/rails_app/config/deploy/staging.rb
@@ -0,0 +1,11 @@
+set :runner, '<USER_NAME>'
+set :user, '<USER_NAME>'
+
+set :domain, '<DOMAIN>'
+set :application, '<APPLICATION>'      # eg 'rfx'
+set :deploy_to, '<PATH>'
+
+set :keep_releases, 10
+set :branch, 'staging'
+set :rails_env, "staging"
+set :use_sudo, false
