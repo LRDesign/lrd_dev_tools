0273801e38fb45c463e96b76ae25abd0f9a54f3b
Evan Dorn <evan@lrdesign.com>
2011-05-02 23:02:24 -0700

    fix bugs in git_info.rb

--- lib/templates/rails_app/config/initializers/git_info.rb
+++ lib/templates/rails_app/config/initializers/git_info.rb
@@ -1,10 +1,14 @@
 if Rails.env.development? or Rails.env.staging?
-  GIT_REVISION = `git show --pretty=format:"%H" --quiet`[0..8]
+  GIT_REVISION = `git --no-pager show --pretty=format:"%H" --quiet`[0..8]
   if GIT_REVISION =~ /fatal/
     GIT_REVISION = GIT_TAG = GIT_COMMIT_DATE = "N/A"
   else
-    GIT_COMMIT_DATE = DateTime.parse(`git show --pretty=format:"%ci" --quiet`)
-    GIT_TAG = `git describe 2>&1`
-    GIT_TAG = "no tag" if GIT_TAG =~ /fatal/
+    GIT_COMMIT_DATE = DateTime.parse(`git --no-pager show --pretty=format:"%ci" --quiet`)
+    temp_tag = `git --no-pager describe 2>&1`
+    if temp_tag =~ /fatal/
+      GIT_TAG = "no tag"
+    else
+      GIT_TAG = temp_tag
+    end
   end
 end
