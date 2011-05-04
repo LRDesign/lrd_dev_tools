4a5673cb1c79ec88d3d4881234f6b9a7e5b708f5
Judson <nyarly@gmail.com>
2011-05-03 17:15:05 -0700

    Small fixes for database.yml and git_info

--- lib/templates/rails_app/config/initializers/git_info.rb
+++ lib/templates/rails_app/config/initializers/git_info.rb
@@ -1,8 +1,9 @@
 if Rails.env.development? or Rails.env.staging?
-  GIT_REVISION = `git --no-pager show --pretty=format:"%H" --quiet`[0..8]
-  if GIT_REVISION =~ /fatal/
-    GIT_REVISION = GIT_TAG = GIT_COMMIT_DATE = "N/A"
-  else
+  def git_command(command)
+    return `git --no-pager #{command} 2>&1`
+  end
+  GIT_REVISION = git_command('show --pretty=format:"%H" --quiet')[0..8]
+  begin
     GIT_COMMIT_DATE = DateTime.parse(`git --no-pager show --pretty=format:"%ci" --quiet`)
     temp_tag = `git --no-pager describe 2>&1`
     if temp_tag =~ /fatal/
@@ -10,5 +11,7 @@ if Rails.env.development? or Rails.env.staging?
     else
       GIT_TAG = temp_tag
     end
+  rescue ArgumentError #can't parse date
+    GIT_TAG = GIT_COMMIT_DATE = "N/A"
   end
 end
