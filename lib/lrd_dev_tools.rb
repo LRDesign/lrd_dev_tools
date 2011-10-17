9708bea28e055f023e069d23569cec18b3797c85
Evan Dorn <evan@lrdesign.com>
2011-10-16 20:47:57 -0700

    added initializer to configure all the scaffold generators for us

--- lib/lrd_dev_tools.rb
+++ lib/lrd_dev_tools.rb
@@ -1,11 +1,31 @@
+p "************************************************************** loaded LRD Dev Tools"
+
 module LRD::DevTools
   class Railtie < Rails::Railtie
     config.generators do |g|
       g.templates.unshift File::expand_path('../templates', __FILE__)
     end
 
+    # We have to configure our test_framework and fixture_replacement in an initializer
+    # because FactoryGirl rudely overwrites any test_framework that doesn't == :rspec with
+    # :test_unit.  Boo.
+    initializer "lrd_dev_tools.set_generators", :after => 'factory_girl.set_fixture_replacement' do
+      generators = config.respond_to?(:app_generators) ? config.app_generators : config.generators
+      generators.template_engine     'mizugumo:haml'
+      generators.scaffold_controller 'mizugumo:scaffold_controller'
+      generators.assets              'mizugumo:js_assets'
+
+      generators.test_framework :lrdspec, :fixture => true
+      generators.fixture_replacement 'lrdspec:factory'      
+
+      generators.fallbacks['mizugumo:haml']  = :haml
+      generators.fallbacks[:lrdspec] = :rspec
+    end   
+
     rake_tasks do
       Dir[File.expand_path("../tasks/**/*.rake", __FILE__)].each {|f| load f }
     end
   end
 end
+
+
